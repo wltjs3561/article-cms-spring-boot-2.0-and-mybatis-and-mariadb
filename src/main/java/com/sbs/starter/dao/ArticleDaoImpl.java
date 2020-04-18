2e04aafe62f2f81f439c312a9a4fd8080a2f32b1
wltjs3561 <wltjs3561@gmail.com>
2020-04-18 13:32:50 +0900

    ArticleDao생성 및 적용

---
+++ src/main/java/com/sbs/starter/dao/ArticleDaoImpl.java
@@ -0,0 +1,27 @@
+package com.sbs.starter.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sbs.starter.dto.Article;
+
+@Component //이렇게 해야 AutoWired에 의한 연결에 적용된다.
+public class ArticleDaoImpl implements ArticleDao{
+	public List<Article> getList(){
+		//중간확인용 가짜 데이터
+		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용1");
+		Article article2 = new Article(2, "2019-08-20 12:12:12", "제목2", "내용2");
+		Article article3 = new Article(3, "2019-08-20 12:12:12", "제목3", "내용3");
+		
+		List<Article> list = new ArrayList<Article>();
+		
+		list.add(article3);
+		list.add(article2);
+		list.add(article1);
+		
+		return list;
+		
+	}
+}
