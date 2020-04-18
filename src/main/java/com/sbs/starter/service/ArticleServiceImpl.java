10e576f1ebb8f123eec194c95d3d27fba28df853
wltjs3561 <wltjs3561@gmail.com>
2020-04-18 14:25:02 +0900

    doADd 서비스 구

--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -2,6 +2,7 @@ package com.sbs.starter.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,11 @@ public class ArticleServiceImpl implements ArticleService{
 	// 이제 ArticleService는 데이터 관련해서는 모두 Dao에게 위임
 	public List<Article> getList(){
 		return articleDao.getList();
+	}
+	
+	@Override
+	public void add(Map<String, Object> param) {
+		articleDao.add(param);
 	}
 	
 }
